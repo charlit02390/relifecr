a4199407d9bcddfb79b7326ab66974285fb67a0b
charlit02390 <cmonge@jackdevelopers.com>
2018-08-16 13:39:24 -0600

    Delete passwords

--- server/configuration/knex-config.js
+++ server/configuration/knex-config.js
@@ -4,14 +4,14 @@ var logger = require('winston');
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : 'ec2-52-36-200-181.us-west-2.compute.amazonaws.com',
-    user : 'root',
-    password : 'root',
-    database : 'relifebd'
+    host : '',
+    user : '',
+    password : '',
+    database : ''
   }
 });
 
-logger.info('EDB - Connecting to localhost as root');
+logger.info('Relife - Connecting to localhost as root');
 
 knex.on('disconnect', function(error) {
     if (error) {
